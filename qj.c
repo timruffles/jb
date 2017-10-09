1c39df244d7341d9fc895ff316a405f3b50631cb
Tim Ruffles <truffles@cloudflare.com>
2017-10-09 08:11:25 +0100

    Joined up input

--- qj.c
+++ qj.c
@@ -1,6 +1,9 @@
 # include <stdio.h>
 # include <stdbool.h>
 # include <stdlib.h>
+# include <stdint.h>
+# include <regex.h>
+# include <string.h>
 
 # define EOS '\0'
 
@@ -17,8 +20,61 @@ static int argc;
 
 typedef char OperatorLiteral[3];
 
+static const char INPUT_TOO_LARGE[] = "Input too large";
+
+//// Take the next N characters. Will update TakeResult with
+//// flags
+//struct TakeResultT {
+//  // was the request filled completely?
+//  bool filled,
+//  // how many taken
+//  uint8_t n
+//} TakeResult;
+//
+//char* take(uint8_t n) {
+//}
+//
+//char* takeExact(char* string) {
+//}
+//
+char* stringJoin(uint8_t count, char* strings[]);
 
 int main(int argc_, char* argv_[]) {
+  char* input = stringJoin(argc_, argv_);
+  puts(input);
+  regex_t result;
+  regmatch_t operatorMatch[1];
+  uint8_t matchCount = 1;
+  if(regexec(&result, input, matchCount,
+        operatorMatch, REG_EXTENDED | REG_ICASE)) {
+  }
+}
+
+void assert(bool t, const char* msg) {
+  if(!t) {
+    fputs(msg, stderr);
+    exit(1);
+  }
+}
+
+// returns pointer to new string which is concatentation of
+// all other strings
+char* stringJoin(uint8_t n, char* strings[]) {
+  uintmax_t total = 0;
+  for(uint8_t i = 0; i < n; i++) {
+    total += (uintmax_t) strlen(strings[i]);
+    // ensure we don't blow total
+    assert(total <= INTMAX_MAX, &INPUT_TOO_LARGE[0]);
+  }
+  char* joined = calloc(total, sizeof (char));
+  for(uint8_t i = 0; i < n; i++) {
+    strcat(joined, strings[i]);
+  }
+  return joined;
+}
+
+
+int main2(int argc_, char* argv_[]) {
   argc = argc_;
   argv = (char**) argv_;
   pickInput();
