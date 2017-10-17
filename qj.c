54063f658837bfa487ec6446c97f33efd1c5342f
Tim Ruffles <truffles@cloudflare.com>
2017-10-17 22:57:29 +0100

    getting somewhere non seg-faulty

--- qj.c
+++ qj.c
@@ -9,18 +9,22 @@
 // variables are initialized to 0
 # define UNINITIALIZED 0;
 # define callocOne(A) calloc(1, A);
+# define debugf(format, ...) fprintf (stderr, "DEBUG:%d " format "\n", __LINE__, __VA_ARGS__)
+# define debug(msg) fprintf (stderr, "DEBUG:%d " msg "\n", __LINE__)
+# define assert(t, msg) if(!(t)) { fprintf (stderr, "ASSERT:%d " msg "\n", __LINE__); }
 
 
 static char* input;
-
+static uintmax_t inputLength;
+// where are we in the input?
+static uintmax_t inputOffset;
 
 typedef char OperatorLiteral[3];
 char OPERATOR_RE[] = "([oasc])([oce])( |$)";
 
-static const char INPUT_TOO_LARGE[] = "Input too large";
 void bug(bool, const char*);
 
-char* stringJoin(uint8_t count, char* strings[]);
+char* stringJoin(uint8_t count, char* strings[], uintmax_t* size);
 
 void output(char* string) {
   puts(string);
@@ -28,13 +32,6 @@ void output(char* string) {
 
 void outputc(char* string) {
   puts(string);
-}
-
-void assert(bool t, const char* msg) {
-  if(!t) {
-    fputs(msg, stderr);
-    exit(1);
-  }
 }
 
 
@@ -47,6 +44,7 @@ enum ActionKind {
     CloseAction = 'c',
     EmptyAction = 'e'
 };
+int_fast32_t NotActionKind = -1;
 
 enum ExpressionKind {
     EscapeToken,
@@ -79,6 +77,10 @@ union Expression {
 };
 
 
+struct ParseResultS {
+  bool success;
+  char* errorMessage;
+};
 
 
 // we pass around nodes as pointers
@@ -90,7 +92,6 @@ static ParseResult expressionParse(ExpressionNode* node);
 void objectEmpty() {
   output("{}");
 }
-
 
 static enum ExpressionKind getKind(ExpressionNode* node) {
   // TODO should this guard against null ptrs?
@@ -106,7 +107,9 @@ static bool isStringNode(ExpressionNode* node) {
   }
 }
 
+
 static ParseResult objectPairParse(enum ActionKind action, ExpressionNode* result) {
+  debug("object pair parse");
   switch(action) {
     // interesting note: as first line in case is an initialization (bool isKey =...)
     // we need to create a bracket statement to jump to
@@ -114,7 +117,12 @@ static ParseResult objectPairParse(enum ActionKind action, ExpressionNode* resul
       bool isKey = true;
       while(true) {
         ExpressionNode* next = callocOne(sizeof (ExpressionNode));
-        expressionParse(next);
+        bool parsed = expressionParse(next);
+        if(!parsed && !isKey) {
+          // if we've parsed a key, we need a value too
+          return false;
+        }
+
         if(isKey && !isStringNode(next)) {
           return false;
         }
@@ -130,11 +138,39 @@ static ParseResult objectPairParse(enum ActionKind action, ExpressionNode* resul
       // FIXME
       result = NULL;
       return false;
-    case EmptyAction:
-      return false;
+    case EmptyAction: {
+      const struct ObjectPairExpression pair = {
+        .kind = ObjectPair,
+      };
+
+      result->objectPair = pair;
+      return true;
+    }
+
   }
 }
 
+static void advance(uint32_t n) {
+  uintmax_t m = n + inputOffset ;
+  inputOffset = m >= inputLength
+    ? inputLength
+    : m;
+}
+
+static bool isEof() {
+  return inputOffset >= inputLength;
+}
+
+static enum ActionKind parseActionKind(char kind) {
+  switch(kind) {
+    case OpenAction:
+    case CloseAction:
+    case EmptyAction:
+      return kind;
+    default:
+      return NotActionKind;
+  }
+}
 
 static ParseResult pairParse(ExpressionNode* result) {
   static bool compiled = false;
@@ -143,14 +179,25 @@ static ParseResult pairParse(ExpressionNode* result) {
   if(!compiled) {
     bug(!regcomp(&operatorRe, OPERATOR_RE, REG_EXTENDED | REG_ICASE), "One of the language regexps could not be compiled");
     compiled = true;
+    debug("parsed operatorRe");
   }
 
-  regmatch_t operatorMatch[1];
-  uint8_t matchCount = 1;
-  if(regexec(&operatorRe, input, matchCount,
-        operatorMatch, REG_EXTENDED | REG_ICASE)) {
+
+  uint8_t matchCount = 3;
+  regmatch_t operatorMatch[matchCount];
+  int reResult = regexec(&operatorRe, &input[inputOffset], matchCount,
+        operatorMatch, 0);
+  debugf("result matching pair vs '%s' token '%i'", input, reResult);
+
+  if(reResult != REG_NOMATCH) {
+    advance(2);
     const enum ExpressionKind kind = input[operatorMatch[0].rm_so];
-    const enum ActionKind action = input[operatorMatch[1].rm_so];
+    const enum ActionKind action = parseActionKind(input[operatorMatch[1].rm_so]);
+    if(action == NotActionKind) {
+      return false;
+    }
+
+    debugf("parsed '%c' pair with '%c' action", kind, action);
     switch(kind) {
       case ObjectPair:
         return objectPairParse(action, result);
@@ -163,6 +210,11 @@ static ParseResult pairParse(ExpressionNode* result) {
 }
 
 static ParseResult expressionParse(ExpressionNode* result) {
+  if(isEof()) {
+    return false;
+  }
+
+  debug("expressionParse()");
   if(pairParse(result)) {
     return true;
   }
@@ -173,10 +225,13 @@ static ParseResult expressionParse(ExpressionNode* result) {
 
 
 int main(int argc_, char* argv_[]) {
+  // disable buffering for debuggability
+  setbuf(stdout, NULL);
+
   // TODO - support stdin
   // ignore argv[0] - program name
-  input = stringJoin(argc_ - 1, &argv_[1]);
-  puts(input);
+  input = stringJoin(argc_ - 1, &argv_[1], &inputLength);
+  debugf("parsing '%s' as jb", input);
 
   ExpressionNode* expression = callocOne(sizeof (union Expression));
   assert(expressionParse(expression), "Failed to parse");
@@ -195,14 +250,15 @@ void bug(bool t, const char* msg) {
 
 // returns pointer to new string which is concatentation of
 // all other strings
-char* stringJoin(uint8_t n, char* strings[]) {
+char* stringJoin(uint8_t n, char* strings[], uintmax_t* size) {
   uintmax_t total = 0;
   for(uint8_t i = 0; i < n; i++) {
     total += (uintmax_t) strlen(strings[i]);
     // ensure we don't blow total
-    assert(total <= INTMAX_MAX, &INPUT_TOO_LARGE[0]);
+    assert(total <= INTMAX_MAX, "Input too large");
   }
   char* joined = calloc(total, sizeof (char));
+  *size = total;
   for(uint8_t i = 0; i < n; i++) {
     strcat(joined, strings[i]);
   }
